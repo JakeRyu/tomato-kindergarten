eda7c2c71822a123d76034a15a5e53d6202b8979
Jake Ryu <jake.ryu@hotmail.co.uk>
2017-08-31 23:14:26 +0100

    Add DbContext

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using TomatoKindergarten.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 namespace TomatoKindergarten
 {
@@ -22,6 +24,8 @@ namespace TomatoKindergarten
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options => 
+                options.UseSqlServer(Configuration.GetConnectionString("Default")));
             services.AddMvc();
         }
 
