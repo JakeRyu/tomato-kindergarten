991915db3a9e0c816897215cce5025327dfec4a5
Jake Ryu <jake.ryu@hotmail.co.uk>
2017-09-01 08:40:32 +0100

    Add AutoMapper

---
+++ Mapping/MappingProfile.cs
@@ -0,0 +1,13 @@
+using AutoMapper;
+using TomatoKindergarten.Core.Models;
+
+namespace TomatoKindergarten.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // CreateMap<Group, GroupResource>();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using TomatoKindergarten.Persistence;
 using Microsoft.EntityFrameworkCore;
+using AutoMapper;
 
 namespace TomatoKindergarten
 {
@@ -24,6 +25,8 @@ namespace TomatoKindergarten
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAutoMapper();
+            
             services.AddDbContext<AppDbContext>(options => 
                 options.UseSqlServer(Configuration.GetConnectionString("Default")));
             services.AddMvc();
